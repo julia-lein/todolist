077abba253fdbafbd8247ac3b87d9d0bdb34f67a
Julia Lein <julia.lein77@gmail.com>
2021-04-15 17:42:46 +0200

    Project done

---
+++ scripts/addtask.js
@@ -0,0 +1,49 @@
+function addTask(newTaskInput, tasksList)
+{
+    const newTask = newTaskInput.value;
+    newTaskInput.value = "";
+
+    if (newTask.trim() !== "")
+    {
+        //create new li item
+        const listElement = document.createElement("li");
+            
+        //create check button (done)
+        addItemToDom("span", "check", "no-value", "no-attribute", listElement);
+
+        //create item with task's text
+        addItemToDom("input", "task", newTask, "disabled", listElement);
+
+        //create edit button
+        addItemToDom("button", "edit", "no-value", "no-attribute", listElement);
+
+        //create delete button
+        addItemToDom("button", "delete", "no-value", "no-attribute", listElement);
+
+        tasksList.prepend(listElement);
+
+        document.querySelector(".no-tasks").classList.add("yes-tasks");
+    }
+
+}
+
+
+function addItemToDom(itemName, itemClass, value, attribute, parentItem)
+{
+    const newItem = document.createElement(itemName);
+    newItem.classList.add(itemClass);
+
+    if (value !== "no-value")
+    {
+        newItem.value = value;
+    }
+
+    if (attribute !== "no-attribute")
+    {
+        newItem.setAttribute(attribute, "");
+    }
+
+    parentItem.append(newItem);
+}
+
+export { addTask }
